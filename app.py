b81af03c53e39b66616cb32e2264cef07dc6a059
kitsamho <kitsamho@gmail.com>
2022-03-04 17:11:40 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import sys
+import os
+
+st.set_page_config(layout="wide")
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.title("Happy Birthday! xx")
+st.image('./assets/voucher.png')
+
+
+st.write("(I think they have some introductory classes, probably best call them to ask. :) )")
+
+
+
